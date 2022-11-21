3851d490b6848e4943d919659126ecadf0dbca67
Cobie French <chf19@fsu.edu>
2022-11-21 12:25:45 -0500

    body image

--- sketch.js
+++ sketch.js
@@ -1,7 +1,11 @@
+let img;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(500, 700);
+  img = loadImage('Human.png')
 }
 
 function draw() {
   background(125);
+  image(img, 0,0);
 }
