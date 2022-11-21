b8aad8259944c42e5fd2da996f0ce9c5706715e9
Cobie French <chf19@fsu.edu>
2022-11-21 13:29:28 -0500

    made circle for viewport

--- sketch.js
+++ sketch.js
@@ -2,10 +2,11 @@ let img;
 
 function setup() {
   createCanvas(500, 700);
-  img = loadImage('Human.png')
+  img = loadImage('assets/human.png')
 }
 
 function draw() {
   background(125);
   image(img, 0,0);
+  circle(mouseX, mouseY, 50);
 }
